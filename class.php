9e4cd87e99534775ffac705918f317be1e838d98
mundabenj <benjamin.mundama@strathmore.edu>
2025-09-01 09:23:18 +0300

    create basic class

---
+++ class.php
@@ -0,0 +1,12 @@
+<?php
+
+// create a class
+class Sample {
+    public function greet() {
+        return "Hello class! <br>";
+    }
+    public function week_day() {
+        return "Today is " . date("l");
+    }
+}
+
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-print "Hello, World!";
+// Include the class file
+require 'class.php';
 
-?>
+//call the class
+//create an instance
+$sample = new Sample();
+
+// call the greet method
+echo $sample->greet();
+
+// call the week_day method
+echo $sample->week_day();
